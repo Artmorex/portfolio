05d79ed7d2c0b2e7736feacb4c7a59e313bca651
Artem Zapp <artmore111@ukr.net>
2021-01-10 14:20:42 +0200

    add d3js project

--- scripts/data.js
+++ scripts/data.js
@@ -10,19 +10,19 @@ var data = [
 	},
 	{
 		id: 1,
-		title: "Meta Parser",
-		description: "Parse Youtube via YouTube Analytics and Reporting APIs",
-		linkToDemo: "http://mkozhukharenko.github.io/",
-		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io",
+		title: "D3.js Diagramm",
+		description: "NDA Freelance Project",
+		linkToDemo: "",
+		linkToCode: "",
 		image: "images/works-color/this_portfolio.jpg",
-		tags: ["Pure JS", "Pure HTML/CSS", "Less"]
+		tags: ["D3.js", "Pure HTML/CSS", "Pure JS"]
 	},
 	{
 		id: 1,
-		title: "News picker",
-		description: "Simple app that receives the news from the server (Guardian Open API) and parse it to DOM.",
-		linkToDemo: "http://mkozhukharenko.github.io/newspicker/src/index.html#/news",
-		linkToCode: "https://github.com/mkozhukharenko/newspicker",
+		title: "Meta Parser",
+		description: "Parse Youtube via YouTube Analytics and Reporting APIs",
+		linkToDemo: "",
+		linkToCode: "",
 		image: "images/works-color/newspicker.jpg",
 		tags: ["Angular", "Bootstrap", "AJAX"]
 	},
@@ -30,8 +30,8 @@ var data = [
 		id: 2,
 		title: "Questionnary",
 		description: "Multi parent graph presented as a complex questionnaire. This's the solution for qualifying round of Web Challenge 2015.",
-		linkToDemo: "http://mkozhukharenko.github.io/multi-parent-graph/",
-		linkToCode: "https://github.com/mkozhukharenko/multi-parent-graph",
+		linkToDemo: "",
+		linkToCode: "",
 		image: "images/works-color/graph.jpg",
 		tags: ["Pure JS"]
 	},
@@ -39,8 +39,8 @@ var data = [
 		id: 3,
 		title: "Form validation",
 		description: "Form validation using as registration form example. Field 'email' has server side validation.",
-		linkToDemo: "http://mkozhukharenko.github.io/works/form-validation/",
-		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io/tree/master/works/form-validation",
+		linkToDemo: "",
+		linkToCode: "",
 		image: "images/works-color/form-validation.jpg",
 		tags: ["AJAX", "Bootstrap", "Pure JS"]
 	},
@@ -48,8 +48,8 @@ var data = [
 		id: 4,
 		title: "Store mock up",
 		description: "Home page mock up for online store. None css frameworks has used.",
-		linkToDemo: "http://mkozhukharenko.github.io/works/electronic-shop/",
-		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io/tree/master/works/electronic-shop",
+		linkToDemo: "",
+		linkToCode: "",
 		image: "images/works-color/shop.jpg",
 		tags: ["Pure HTML/CSS"]
 	},
@@ -57,8 +57,8 @@ var data = [
 		id: 5,
 		title: "Timer",
 		description: "Several timers can be inserted on one page: code has bulid on prototypes. No frameforks.",
-		linkToDemo: "http://mkozhukharenko.github.io/works/timer",
-		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io/tree/master/works/timer",
+		linkToDemo: "",
+		linkToCode: "",
 		image: "images/works-color/timer.jpg",
 		tags: ["Pure JS"]
 	},
@@ -66,8 +66,8 @@ var data = [
 		id: 6,
 		title: "Zoomable image",
 		description: "Fully adaptive zoomable image (fit all screens). Code is reusable.",
-		linkToDemo: "http://mkozhukharenko.github.io/works/zoomable/",
-		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io/tree/master/works/zoomable",
+		linkToDemo: "",
+		linkToCode: "",
 		image: "images/works-color/zoomable.jpg",
 		tags: ["jQuery", "Pure HTML/CSS"]
 	},
@@ -75,8 +75,8 @@ var data = [
 		id: 7,
 		title: "Slider",
 		description: "Can be reusable (several sliders on one page). Slide fit all the screens. Build using MPC pattern.",
-		linkToDemo: "http://mkozhukharenko.github.io/works/slider/",
-		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io/tree/master/works/slider",
+		linkToDemo: "",
+		linkToCode: "",
 		image: "images/works-color/slider.jpg",
 		tags: ["jQuery", "Pure HTML/CSS"]
 	},
@@ -84,8 +84,8 @@ var data = [
 		id: 8,
 		title: "Portfolio mock up",
 		description: "Mock up for portfolio. None css frameworks has used.",
-		linkToDemo: "http://mkozhukharenko.github.io/works/portfolio/",
-		linkToCode: "https://github.com/mkozhukharenko/mkozhukharenko.github.io/tree/master/works/portfolio",
+		linkToDemo: "",
+		linkToCode: "",
 		image: "images/works-color/portfolio.jpg",
 		tags: ["Pure HTML/CSS"]
 	}
