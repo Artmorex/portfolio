b896f48aae2deef372cd45c40ebd70cb3c120b3e
Artem <artmorex@gmail.com>
2024-01-08 16:17:53 +0200

    Update send.php

--- send.php
+++ send.php
@@ -15,7 +15,7 @@ $fio = trim($fio);
 $email = trim($email);
 $message = trim($message);
 
-if (mail("admin@artmorex.com", "My Dear, myself, you have a new message from artmorex.com", "Имя:".$fio.". E-mail: ".$email, "Текст: ".$message)) { 
+if (mail("artmorex@gmail.com", "My Dear, myself, you have a new message from artmorex.com", "Имя:".$fio.". E-mail: ".$email, "Текст: ".$message)) { 
 	echo "Your message successfully send!";
 } else {
 	echo "There was an error sending your message! Please contact artmorex@gmail.com";
